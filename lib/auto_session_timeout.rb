9b4b4487a6335bddbe916f69cbbbef50486cb883
Chris Irish <irish@velocitylabs.io>
2017-08-17 12:52:44 -0700

    refactor for readability

--- lib/auto_session_timeout.rb
+++ lib/auto_session_timeout.rb
@@ -1,40 +1,46 @@
 module AutoSessionTimeout
-  
   def self.included(controller)
     controller.extend ClassMethods
   end
-  
+
   module ClassMethods
-    def auto_session_timeout(seconds=nil, sign_in_path)
+    def auto_session_timeout(seconds = nil, sign_in_path = nil)
       protect_from_forgery except: [:active, :timeout]
       prepend_before_action do |c|
-        if c.session[:auto_session_expires_at] && c.session[:auto_session_expires_at] < Time.now && (sign_in_path ? !(c.env["PATH_INFO"] == sign_in_path && c.env["REQUEST_METHOD"] == "POST") : true)
+        sign_out = if sign_in_path && request.fullpath == sign_in_path && request.method_symbol == :post
+                         false
+                       else
+                         true
+                       end
+
+        if c.session[:auto_session_expires_at] && c.session[:auto_session_expires_at] < Time.current && sign_out
+
           c.send :reset_session
         else
           unless c.request.original_url.start_with?(c.send(:active_url))
             offset = seconds || (current_user.respond_to?(:auto_timeout) ? current_user.auto_timeout : nil)
-            c.session[:auto_session_expires_at] = Time.now + offset if offset && offset > 0
+            c.session[:auto_session_expires_at] = Time.current + offset if offset && offset > 0
           end
         end
       end
     end
-    
+
     def auto_session_timeout_actions
       define_method(:active) { render_session_status }
       define_method(:timeout) { render_session_timeout }
     end
   end
-  
+
   def render_session_status
     response.headers["Etag"] = ""  # clear etags to prevent caching
     render plain: !!current_user, status: 200
   end
-  
+
   def render_session_timeout
     flash[:notice] = "Your session has timed out."
     redirect_to "/login"
   end
-  
+
 end
 
 ActionController::Base.send :include, AutoSessionTimeout
